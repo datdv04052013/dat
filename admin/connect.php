1d00d53f6ab9fb6c9fb85bf57cd262b69bdd702d
datdvbhaf190049 <62133623+datdvbhaf190049@users.noreply.github.com>
2021-01-19 15:59:59 +0700

    fix file sql 2

--- admin/connect.php
+++ admin/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli('34.200.32.20', 'root', '', 'musicshop');
+$con = new mysqli('34.200.32.20', 'musicshop_user'@'%', '', 'musicshop');
 
 // Check connection
 if (mysqli_connect_errno()) {
